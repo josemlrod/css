fa96af8a78096f42da811b19ce527d767f018d3b
josemlrod <mrjosemlrod@gmail.com>
2026-06-12 23:42:37 -0400

    Update favicon and remove Inter

--- app/root.tsx
+++ app/root.tsx
@@ -13,15 +13,22 @@ import type { Route } from './+types/root';
 import './app.css';
 
 export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/cropped-icon-32x32.png',
   },
   {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '192x192',
+    href: '/cropped-icon-192x192.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '192x192',
+    href: '/cropped-icon-192x192.png',
   },
 ];
 
